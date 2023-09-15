7d0d46cfd9534516926ed4bf10c622bed88974b1
jscheu <joshua.scheu@gmail.com>
2023-09-15 12:34:51 -0500

    Finished exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,9 +11,12 @@ const Button = ({text, handleClick}) => {
   )
 }
 
-const Statistic = ({name, count}) => {
-  return (
-    <p>{name} {count}</p>
+const Statistic = ({type, name, value}) => {
+  if (type === "percent") return (
+    <p>{name} {value} %</p>
+  )
+  else return (
+    <p>{name} {value}</p>
   )
 }
 
@@ -22,6 +25,12 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
+
+  const average = total === 0 ? 0 : (good - bad)/total
+
+  const percPositive = total === 0 ? 0 : (100 * good)/total
 
   const generateHandleClick = (feedbackType, setterFunc) => () => {
     console.log(`The 'setFeedback' function is being called with feedback of '${feedbackType}'`)
@@ -42,9 +51,13 @@ const App = () => {
       </div>
       <div>
         <Header title={statisticsTitle}/>
-        <Statistic name="good" count={good}/>
-        <Statistic name="neutral" count={neutral}/>
-        <Statistic name="bad" count={bad}/>
+        <Statistic name="good" value={good}/>
+        <Statistic name="neutral" value={neutral}/>
+        <Statistic name="bad" value={bad}/>
+        <Statistic name="all" value={total} />
+        <Statistic name="average" value={average} />
+        <Statistic type="percent" name="positive" value={percPositive} />
+
       </div>
     </div>
   )
