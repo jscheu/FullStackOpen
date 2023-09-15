29e28cd4ba06cca3fd82c3d1d998ac5b2648f743
jscheu <joshua.scheu@gmail.com>
2023-09-15 14:49:00 -0500

    Finished exercise 1.12

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -58,11 +58,22 @@ const Statistics = ({good, neutral, bad}) => {
 }
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [selected, setSelected] = useState(0)
+  // click handler logic
   const generateHandleClick = (feedbackType, setterFunc) => () => {
     console.log(`The 'setFeedback' function is being called with feedback of '${feedbackType}'`)
     setterFunc(prevCount => prevCount + 1)
@@ -71,6 +82,11 @@ const App = () => {
   const handleGoodClick = generateHandleClick("good", setGood)
   const handleNeutralClick = generateHandleClick("neutral", setNeutral)
   const handleBadClick = generateHandleClick("bad", setBad)
+
+  const handleAnecdoteClick = () => {
+    const rand = Math.floor(Math.random() * anecdotes.length)
+    setSelected(rand)
+  }
 
   return (
     <div>
@@ -81,6 +97,10 @@ const App = () => {
         <Button text="bad" handleClick={handleBadClick} />
       </div>
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <div>
+        <p>{anecdotes[selected]}</p>
+        <Button text="next anecdote" handleClick={handleAnecdoteClick}/>
+      </div>
     </div>
   )
 }
