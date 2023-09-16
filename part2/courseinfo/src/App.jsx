851fc3aaf86542a87e23f172f24599f330a53c2b
jscheu <joshua.scheu@gmail.com>
2023-09-16 10:52:00 -0500

    Finished 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,29 +1,4 @@
-const Header = ({ text }) => <h1>{text}</h1>
-
-const Total = ({ sum }) => <p><b>total of {sum} exercises</b></p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    {parts.map(part => <Part key={part.id} part={part}/>)}
-  </>
-
-const Course = ({ course }) => {
-  const parts = course.parts
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <div>
-    <Header text={course.name} />
-    <Content parts={parts} />
-    <Total sum={totalExercises} />
-  </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
