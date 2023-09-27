9ea5a1a83fdda15c53d65a1725b4bafb8481b2ff
jscheu <joshua.scheu@gmail.com>
2023-09-26 20:24:02 -0500

    updated phonebook to use variable paths for dev/production

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,23 +1,24 @@
 import axios from 'axios'
-const baseUrl = '/api/persons'
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || ''
+const apiUrl = `${apiBaseUrl}/api/persons`
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
+    const request = axios.get(apiUrl)
     return request.then(response => response.data)
 }
 
 const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
+    const request = axios.post(apiUrl, newObject)
     return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    const request = axios.put(`${apiUrl}/${id}`, newObject)
     return request.then(response => response.data)
 }
 
 const remove = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
+    const request = axios.delete(`${apiUrl}/${id}`)
     return request.then(response => response.data)
 }
 
