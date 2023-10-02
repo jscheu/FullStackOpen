0fccaa5ffa566cd128943a235014cb0905f47aee
jscheu <joshua.scheu@gmail.com>
2023-10-02 17:10:55 -0500

    finished 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,15 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get("/", async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', {
+            username: 1,
+            name: 1
+        })
+
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const user = (await User.find({}))[0]
+
+    const blogData = request.body
+    blogData.user = user.toJSON().id
+
+    const blog = new Blog(blogData)
 
     const result = await blog.save()
+
+    user.blogs = user.blogs.concat(result.toJSON().id)
+    user.markModified('blogs')
+    await user.save()
+
     return response.status(201).json(result)
 })
 
