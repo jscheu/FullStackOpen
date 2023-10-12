0c47e8c402ebbd04af2b565af2aaa24d7ba81ea3
jscheu <joshua.scheu@gmail.com>
2023-10-12 12:00:12 -0500

    finished 6.8

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -7,6 +7,20 @@ const anecdotesAtStart = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: { content }
+  }
+}
+
+export const incrementVote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
+}
+
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,13 +31,29 @@ const asObject = (anecdote) => {
   }
 }
 
+const sortByVotes = (anecdotes) => {
+  return anecdotes.sort((a, b) => b.votes - a.votes)
+}
+
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch(action.type) {
+    case 'VOTE':
+      const id = action.payload.id
+      const stateAfterVote = state.map(a => a.id === id ? { ...a, votes: a.votes + 1 } : a)
+      console.log('new state: ', stateAfterVote)
+      return sortByVotes(stateAfterVote)
+    case 'NEW_ANECDOTE':
+      const anecdote = action.payload.content
+      const stateAfterNew = [...state, asObject(anecdote)]
+      console.log('new state: ', stateAfterNew)
+      return stateAfterNew
+    default: return state
+  }
 }
 
 export default reducer
